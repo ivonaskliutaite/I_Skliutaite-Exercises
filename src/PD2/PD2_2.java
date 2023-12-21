05b37758a5578529e6675e066da0f32974434e0e
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2023-12-21 13:49:08 +0200

    Add PD2_2 exercise solution.

---
+++ src/PD2/PD2_2.java
@@ -0,0 +1,10 @@
+package PD2;
+
+public class PD2_2 {
+    public static void main(String[] args) {
+        System.out.println(" J\t   A\t\t V\t   V\tA");
+        System.out.println(" J\t  A A\t\t  V\t  V\t   A A");
+        System.out.println(" J\t A A A\t\t   V V\t  A A A");
+        System.out.println("JJ\tA\t  A\t\t\tV\t A\t   A");
+    }
+}
