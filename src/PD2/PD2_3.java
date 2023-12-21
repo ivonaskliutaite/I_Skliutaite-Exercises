6b01b249c59e9e20ab4cefcd0136a5675d10a626
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2023-12-21 13:56:58 +0200

    Add PD2_3 exercise solution.

---
+++ src/PD2/PD2_3.java
@@ -0,0 +1,12 @@
+package PD2;
+
+public class PD2_3 {
+    public static void main(String[] args) {
+        System.out.println("a\t\t a^2\t a^3\t a^4");
+        System.out.println("----------------------------");
+        System.out.println("1\t\t 1\t\t 1\t\t 1");
+        System.out.println("2\t\t 4\t\t 8\t\t 16");
+        System.out.println("3\t\t 9\t\t 27\t\t 81");
+        System.out.println("4\t\t 16\t\t 64\t\t 256");
+    }
+}
