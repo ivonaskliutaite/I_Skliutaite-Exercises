ef09fc6613a879b2fb3e198a5e147ebab8bf56d1
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2023-12-21 14:07:57 +0200

    Add PD2_5 exercise solution.

---
+++ src/PD2/PD2_5.java
@@ -0,0 +1,13 @@
+package PD2;
+
+public class PD2_5 {
+    /* Multiple
+    * line
+    * comment to try :)
+    * */
+
+    // And don't forget about single line comment :)
+    public static void main(String[] args) {
+        System.out.println("Trying some comments....");
+    }
+}
