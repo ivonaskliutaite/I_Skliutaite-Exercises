181547ed30ab687c8d5b23095413fdf940111483
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-01 14:38:41 +0200

    Add PD3_1 exercise solution.

---
+++ src/PD3/PD3_1.java
@@ -0,0 +1,23 @@
+package PD3;
+
+public class PD3_1 {
+    public static void main(String[] args) {
+        byte exampleByte = 122;
+        short exampleShort = 333;
+        int exampleInt = 241414;
+        long exampleLong = 343242L;
+        float exampleFloat = 23432532F;
+        double exampleDouble = 2325.44231;
+        boolean exampleBoolean = false;
+        char exampleChar = 'c';
+
+        System.out.println(exampleByte);
+        System.out.println(exampleShort);
+        System.out.println(exampleInt);
+        System.out.println(exampleLong);
+        System.out.println(exampleFloat);
+        System.out.println(exampleDouble);
+        System.out.println(exampleBoolean);
+        System.out.println(exampleChar);
+    }
+}
