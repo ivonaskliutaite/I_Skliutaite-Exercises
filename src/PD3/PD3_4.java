2ab5b7db77ebfc6d69199e9beff634e9a404f3d4
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-06 21:23:15 +0200

    Add PD3_4 exercise solution.

---
+++ src/PD3/PD3_4.java
@@ -0,0 +1,18 @@
+/*
+ * Sukurkite programą, kuri atlieka keletą
+ * loginių operacijų su boolean kintamaisiais ir atspausdina rezultatus.
+ */
+
+package PD3;
+
+public class PD3_4 {
+    public static void main(String[] args) {
+        boolean trueBoolean = true;
+        boolean defaultBoolean = false;
+        boolean falseBoolean = !true;
+
+        System.out.println("True: " + trueBoolean);
+        System.out.println("False: " + falseBoolean);
+        System.out.println("Default(false): " + defaultBoolean);
+    }
+}
