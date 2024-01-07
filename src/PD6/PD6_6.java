0caa862ce53d8cf63b5f3fe580751d27e744a10a
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-07 21:15:28 +0200

    Add PD6_06 exercise solution.

---
+++ src/PD6/PD6_6.java
@@ -0,0 +1,16 @@
+/*
+Parašykite programą, kuri atspausdina pateikto masyvo elementus.
+*/
+
+package PD6;
+
+public class PD6_6 {
+    public static void main(String[] args) {
+        String [] array = {"lietus", "saule", "zeme", "sniegas"};
+
+        System.out.println("Masyvo elementai: ");
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + "; ");
+        }
+    }
+}
