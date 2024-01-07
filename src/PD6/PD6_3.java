3bd879a9de2395e07d3baa3e511ff264a504e7c8
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-07 20:00:14 +0200

    Add PD6_03 exercise solution.

---
+++ src/PD6/PD6_3.java
@@ -0,0 +1,14 @@
+/*
+Parašykite programą, kuri paverčia masyvą simbolių eilute (String).
+*/
+
+package PD6;
+
+import java.util.Arrays;
+
+public class PD6_3 {
+    public static void main(String[] args) {
+        int [] array = {1, 3, 5, 7};
+        System.out.println("Masyvas: " + Arrays.toString(array));
+    }
+}
