4318478352581bb7eb1a29b5a50ad054519cf563
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-07 20:06:42 +0200

    Correct PD6_03 exercise solution.

--- src/PD6/PD6_3.java
+++ src/PD6/PD6_3.java
@@ -10,5 +10,10 @@ public class PD6_3 {
     public static void main(String[] args) {
         int [] array = {1, 3, 5, 7};
         System.out.println("Masyvas: " + Arrays.toString(array));
+
+        System.out.print("Masyvas: ");
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
     }
 }
