3382f76c8c8808459e29d94ea9c2b541d3aa18db
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-07 03:34:53 +0200

    Add PD4_5 exercise solution.

---
+++ src/PD4/PD4_5.java
@@ -0,0 +1,15 @@
+/*
+Parašykite programą, kuri naudoja ternary operatorių ir atspausdina rezultatą.
+*/
+
+package PD4;
+
+public class PD4_5 {
+    public static void main(String[] args) {
+        int age = 18;
+
+        String msg = age >= 18 ? "Adult." : "Sorry, you're still a kid.";
+
+        System.out.println(msg);
+    }
+}
