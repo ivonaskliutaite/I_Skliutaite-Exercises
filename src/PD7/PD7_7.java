3b106e3c3b2188ff8023546d37625034ab866902
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-13 22:12:28 +0200

    Correct PD7_7 exercise solution.

--- src/PD7/PD7_7.java
+++ src/PD7/PD7_7.java
@@ -6,28 +6,19 @@ package PD7;
 
 public class PD7_7 {
     public static void main(String[] args) {
+        System.out.println(backToFrontNumber(355632));
+    }
 
-        int num = 1234;
-        // sukuriame dezute naujam skaiciui
+    public static String backToFrontNumber(int number) {
         int reversed = 0;
+        System.out.println("Musu pateiktas skaicius: " + number);
 
-        System.out.println("Musu pateiktas skaicius: " + num);
-
-        // ciklas vyks kol num nelygus 0
-        while (num != 0) {
-            // vyksta skaiciaus padalinimas is galo - noredami gauti paskutini skaiciu
-            // musu pateikta skaiciu daliname kaskart is 10
-            int digit = num % 10;
-            // i dezute ikeliamas skaicius
-            // reversed = 0 * 10 + 1234 % 10 = 0 + 4 = 4
-            // reversed = 4
+        while (number != 0) {
+            int digit = number % 10;
             reversed = reversed * 10 + digit;
-
-            // istryname is galo paskutini skaiciu, kuris jau yra reversed dezuteje
-            // num = 1234 / 10 = 123,4
-            // num = 123 / nes liekana neieis i skaiciu, tik sveikas skaicius liks
-            num /= 10;
+            number /= 10;
         }
-        System.out.println("Apsuktas skaicius: " + reversed);
+        String result = "Reversed number = ";
+        return result + reversed;
     }
 }
