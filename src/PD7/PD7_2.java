d68514f9256fdfd9c899c0b21c2cb5e59efa3b42
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-13 20:59:59 +0200

    Correct PD7_2 exercise solution.

--- src/PD7/PD7_2.java
+++ src/PD7/PD7_2.java
@@ -6,10 +6,11 @@ package PD7;
 
 public class PD7_2 {
     public static void main(String[] args) {
-        System.out.println("Converted from C to F: " + convertCelToFar(15.6));
+        System.out.println("Konvertacija is C i F: " + convertCelToFar(15.6));
     }
 
     public static double convertCelToFar(double temp) {
+        System.out.println("Temperatura Celsiumi = " + temp);
         return temp * 1.8 + 32;
     }
 }
