6ecddef743343e0fad6bc768c53d78eedca5afcf
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2024-01-13 20:48:52 +0200

    Add PD7_2 exercise solution.

--- src/PD7/PD7_2.java
+++ src/PD7/PD7_2.java
@@ -6,10 +6,10 @@ package PD7;
 
 public class PD7_2 {
     public static void main(String[] args) {
-        System.out.println(convertCelToFar(15.6));
+        System.out.println("Converted from C to F: " + convertCelToFar(15.6));
     }
 
-    public static double convertCelToFar (double temp){
+    public static double convertCelToFar(double temp) {
         return temp * 1.8 + 32;
     }
 }
